7554aa227f76a940c6131baca4019f0144f85b02
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-20 07:45:48 +0430

    do conditionals part 1 and 2

--- jsCode.js
+++ jsCode.js
@@ -1,8 +1,10 @@
-var num1 = 5;
-var num2 = 6;
-num1 += num2;
-num1 ++;
-document.write(num1);
+var food = "apple";
 
-var num3 = (4 + 5) * 4;
-document.write(num3);
+if(food == "apple")
+{
+    alert("We ate some food ... apple");
+} else if (food == "meat") {
+    alert("We didn't eat meat");
+} else {
+    alert("We didn't eat those.");
+}
