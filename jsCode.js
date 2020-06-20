854c5dad08a6bfb014fe672a717f2df27829c6bf
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-21 01:00:30 +0430

    String object

--- jsCode.js
+++ jsCode.js
@@ -1,32 +1,9 @@
-var orc = {
-    color: "green",
-    hair:"red",
-    weight: 180,
-    height: 5,
-    stomachFull: true,
-    age: 26,
-    yell: function()
-    {
-        document.write("Orcs are the best!");
-    },
+var hello = "Hello how are you doing? ";
 
-    eat: function()
-    {
-        if(this.stomachFull == true)
-        {
-            document.write("YUM YUM!");
-        } 
-        else 
-        {
-            document.write("Not eating");
-        }
-        
-    }
-};
+hello = hello.toUpperCase();
+var a = hello.length;
+hello = hello.replace("DOING","today");
+hello = hello.bold();
+var largeSentence = hello + a
 
-orc.hair = "purple";
-orc.hair = 1;
-orc.hair2 = "red ";
-document.write(orc.hair2);
-delete orc.hair2;
-document.write(orc.hair2);
+document.write(largeSentence);
