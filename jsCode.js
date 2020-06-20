5dc9a202a5e552862267cbc4b7984b6736705fc0
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-20 17:16:28 +0430

    Objects part 1

--- jsCode.js
+++ jsCode.js
@@ -1,6 +1,30 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var orc = {
+    color: "green",
+    weight: 180,
+    height: 5,
+    stomachFull: true,
+    age: 26,
+    yell: function()
+    {
+        document.write("Orcs are the best!");
+    },
 
-for(var i = 0; i<10; i++)
-{
-    document.write(numbers[i]);
-}
+    eat: function()
+    {
+        if(this.stomachFull == true)
+        {
+            document.write("YUM YUM!");
+        } 
+        else 
+        {
+            document.write("Not eating");
+        }
+        
+    }
+};
+
+var newAge = orc.age + 2;
+
+orc.yell();
+orc.eat();
+document.write(newAge);
