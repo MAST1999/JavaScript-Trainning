bdb69159d54ec8907630eb9bf0f437e62fad5db6
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-21 11:04:25 +0430

    changing Styles using js

--- jsCode.js
+++ jsCode.js
@@ -1,7 +1,7 @@
-var todayDate = new Date();
-
-todayDate.toString();
-todayDate.getFullYear();
-todayDate.setFullYear(2021);
-
-document.write(todayDate);
+function changeStyle()
+{
+    var text = document.getElementById("para1").style.color = "blue";
+    var text = document.getElementById("para1").style.backgroundColor = "red";
+    var text = document.getElementById("para1").style.fontFamily = "CaskaydiaCove Nerd Font"
+    
+}
