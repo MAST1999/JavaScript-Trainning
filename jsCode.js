6c9a3596d4963bb6e1ed2c5141197ddfa1715054
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-22 17:50:38 +0430

    The mouseover event.

--- jsCode.js
+++ jsCode.js
@@ -2,4 +2,8 @@ function changeColor()
 {
     document.getElementById("para1").style.color="blue";
 }    
-    
+
+function backToNormal()
+{
+    document.getElementById("para1").style.color="";
+}
