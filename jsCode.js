1210aed187cb876515cf133106d427a0865ccc9b
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-23 15:17:07 +0430

    add element part 2

--- jsCode.js
+++ jsCode.js
@@ -1,7 +1,14 @@
 function newParagraph()
 {
-    var element = document.createElement("p");
+    //This creates a header.
+    var element2 = document.createElement("h2");
     var main = document.getElementById("main");
+    main.appendChild(element2);
+    var text2 = document.createTextNode("The Battle of Salamis");
+    element2.appendChild(text2);
+
+    //This creates a paragraph.
+    var element = document.createElement("p");
     main.appendChild(element)
     var text = document.createTextNode("The Battle of Salamis was fought between an alliance of Greek cities and the Persian Empire in 480 BC.  The Greeks decisively defeated the Persian navy.");
     element.appendChild(text);
