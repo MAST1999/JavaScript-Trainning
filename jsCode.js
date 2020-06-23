8085f112f84a5b2a0a183e5c1647cb014a455f2e
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-23 16:23:10 +0430

    Remove element

--- jsCode.js
+++ jsCode.js
@@ -1,4 +1,4 @@
-function newParagraph()
+function newParagraph() 
 {
     //This creates a header.
     var element2 = document.createElement("h2");
@@ -12,4 +12,14 @@ function newParagraph()
     main.appendChild(element)
     var text = document.createTextNode("The Battle of Salamis was fought between an alliance of Greek cities and the Persian Empire in 480 BC.  The Greeks decisively defeated the Persian navy.");
     element.appendChild(text);
+}
+
+function removeElements() 
+{
+    var elementH = document.getElementsByTagName("h2")[2];
+    var parent = elementH.parentNode;
+    parent.removeChild(elementH);
+
+    var elementP = document.getElementsByTagName("p")[4];
+    parent.removeChild(elementP);
 }
