acf527436be6a088faca60e1f5e764faf8d06d66
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-23 15:07:28 +0430

    Creating a new element

--- jsCode.js
+++ jsCode.js
@@ -1,9 +1,8 @@
-function changeColor()
+function newParagraph()
 {
-    document.getElementById("para1").style.color="blue";
-}    
-
-function backToNormal()
-{
-    document.getElementById("para1").style.color="";
+    var element = document.createElement("p");
+    var main = document.getElementById("main");
+    main.appendChild(element)
+    var text = document.createTextNode("The Battle of Salamis was fought between an alliance of Greek cities and the Persian Empire in 480 BC.  The Greeks decisively defeated the Persian navy.");
+    element.appendChild(text);
 }
