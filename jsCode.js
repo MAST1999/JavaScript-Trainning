026423b2dc4fe8a3ede47974a1519761c62d6cda
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-24 16:25:21 +0430

    Form Validation Part 4

--- jsCode.js
+++ jsCode.js
@@ -2,11 +2,25 @@ function validateTextbox()
 {
     var box = document.getElementById("name");
     var box2 = document.getElementById("address");
+    
     if (box.value.length < 5 || box2.value.length < 5)
     {
-        box.style.backgroundColor = "red";
-        box2.style.backgroundColor = "red";
         alert("The fields marked in red cannot be blank or less than 5 characters.");
+    }
+    
+    if (box.value.length < 5)
+    {
+        box.style.border = "solid 3px red";
+        box.focus();
+    }
+
+    if (box2.value.length < 5)
+    {
+        box2.style.border = "solid 3px red";
+    }
+
+    if (box.value.length < 5 || box2.value.length < 5)
+    {
         return false;
     }
 }
