601192e4ef3669bdb692335b369e4ef7bd4b80fb
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-25 01:26:34 +0430

    Confirmation box

--- jsCode.js
+++ jsCode.js
@@ -1,12 +1,15 @@
-function changeStyle()
-{
-    var paragraph = document.getElementsByClassName("para1");
-    
-    var firstParagraph = paragraph[0].innerHTML;
-    var secondParagraph = paragraph[1].innerHTML;
-    var addThem = paragraph[2].innerHTML = firstParagraph + secondParagraph;
+function changeStyle() {
+    var confirmation = confirm("These changes are final!")
+    if (confirmation == true) {
+        var paragraph = document.getElementsByClassName("para1");
 
-    var firstParagraph = paragraph[0].innerHTML = "";
-    var secondParagraph = paragraph[1].innerHTML = "";
-    document.getElementById("button").style.visibility = "hidden";
+        var firstParagraph = paragraph[0].innerHTML;
+        var secondParagraph = paragraph[1].innerHTML;
+        var addThem = paragraph[2].innerHTML = firstParagraph + secondParagraph;
+
+        var firstParagraph = paragraph[0].innerHTML = "";
+        var secondParagraph = paragraph[1].innerHTML = "";
+        document.getElementById("button").style.visibility = "hidden";
+    }
+
 }
