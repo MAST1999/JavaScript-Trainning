efa8f9f62d3a903026a01e08b26a3e3e065f80d3
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-24 13:55:22 +0430

    Traversing the DOM Part 2

--- jsCode.js
+++ jsCode.js
@@ -1,8 +1,6 @@
 function changeText()
 {
-    var parent = document.getElementById('main');
-    var child = parent.lastElementChild;
-    child.style.color = "blue"
-
-    // var child = parent.childNodes[3];
+    var child = document.getElementById("p1");
+    var parent = child.parentElement;
+    parent.style.color = "blue";
 }
