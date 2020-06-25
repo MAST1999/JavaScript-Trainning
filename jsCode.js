8a3aa33f58b297e87433401a53f67e1f5c977527
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-25 12:43:36 +0430

    windows object

--- jsCode.js
+++ jsCode.js
@@ -1,13 +1,3 @@
-function car(type, color, miles) {
-    this.type = type;
-    this.color = color;
-    this.miles = miles;
-}
-
-var car1 = new car("Compact", "blue", 6580);
-var car2 = new car("Truck", "red", 397);
-var car3 = new car("SUV", "yellow", 9948);
-
-function changeStyle(){
-    document.getElementById("para").innerHTML = car1.type + " car with " + car1.miles + " miles.  " + car1.color + " is the color.";
+function changePage(){
+    window.location="index.html";
 }
