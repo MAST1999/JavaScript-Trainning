701df53cc02bec5bfdcdbf7ebea5a56abf388db4
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-27 21:15:07 +0430

    Nice and easy switch case!!

--- jsCode.js
+++ jsCode.js
@@ -1,36 +1,37 @@
 function printDay() {
-    var day = document.getElementById("day1").value;
-
-    var resultElement = document.createElement("p");
     var body = document.getElementById("body");
+    if (!document.getElementById("para")) {
+        var resultElement = document.createElement("p");
+        resultElement.id = "para";
+        body.appendChild(resultElement);
+    }
+    var day = document.getElementById("day1").value;
     var dayText = document.createTextNode("");
-    body.appendChild(resultElement);
-    resultElement.appendChild(dayText);
-    dayText.nodeValue = "Day is : ";
-    var paragraph = document.getElementsByTagName("p");
+
+    var paragraph = document.getElementById("para");
 
     switch (Number(day)) {
         case 1:
-            var para1 = paragraph[0].innerHTML = "Day is: Saturday";
+            var para1 = document.getElementById("para").innerHTML = "Day is: Saturday";
             break;
         case 2:
-            para1 = paragraph[0].innerHTML = "Day is: Sunday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Sunday";
             break;
         case 3:
-            para1 = paragraph[0].innerHTML = "Day is: Monday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Monday";
             break;
         case 4:
-            para1 = paragraph[0].innerHTML = "Day is: Tuesday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Tuesday";
             break;
         case 5:
-            para1 = paragraph[0].innerHTML = "Day is: Wednesday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Wednesday";
             break;
         case 6:
-            para1 = paragraph[0].innerHTML = "Day is: Thursday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Thursday";
             break;
         case 7:
-            para1 = paragraph[0].innerHTML = "Day is: Friday";
+            para1 = document.getElementById("para").innerHTML = "Day is: Friday";
             break;
     }
-    document.getElementById("button").style.visibility = "hidden";
+
 }
