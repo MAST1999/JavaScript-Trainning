52528792afd67f085e0e88182a6b55e6ed72bfe4
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-27 14:41:55 +0430

    Working on fix!

--- jsCode.js
+++ jsCode.js
@@ -1,8 +1,11 @@
 function printDay() {
     var day = document.getElementById("day1").value;
+    var resultElement = document.createElement("p");
+    var body = document.getElementById("body");
+    var dayText = document.createTextNode("") ;
     switch (day) {
         case 1: 
-            document.write("<p>Saturday</p>");
+            
             break;
         
     }
