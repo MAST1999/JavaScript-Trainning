47f908e85036994af56fd75aace17bb07082b854
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-30 15:43:05 +0430

    simple beginner code

--- jsCode.js
+++ jsCode.js
@@ -1,36 +1,11 @@
 /*jslint browser:true*/
-function printDay() {
+function fnHello() {
     "use strict";
-    var body = document.getElementById("body");
-    if (!document.getElementById("para")) {
-        var resultElement = document.createElement("p");
-        resultElement.id = "para";
-        body.appendChild(resultElement);
+    if (!document.getElementById("pPara")) {
+        var elBody = document.getElementById("elBody");
+        var elPara = document.createElement("p");
+        elBody.appendChild(elPara);
+        elPara.id = "pPara";
     }
-    var day = document.getElementById("day1").value;
-
-    switch (Number(day)) {
-    case 1:
-        document.getElementById("para").innerHTML = "Day is: Saturday";
-        break;
-    case 2:
-        document.getElementById("para").innerHTML = "Day is: Sunday";
-        break;
-    case 3:
-        document.getElementById("para").innerHTML = "Day is: Monday";
-        break;
-    case 4:
-        document.getElementById("para").innerHTML = "Day is: Tuesday";
-        break;
-    case 5:
-        document.getElementById("para").innerHTML = "Day is: Wednesday";
-        break;
-    case 6:
-        document.getElementById("para").innerHTML = "Day is: Thursday";
-        break;
-    case 7:
-        document.getElementById("para").innerHTML = "Day is: Friday";
-        break;
-    }
-
+    document.getElementById("pPara").innerHTML = "Hello !";
 }
