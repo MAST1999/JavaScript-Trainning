8e953c2d6db230a5f466d2fef48cecd4adc1f887
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-30 23:38:46 +0430

    Radio buttons

--- jsCode.js
+++ jsCode.js
@@ -1,12 +1,20 @@
 /*jslint browser:true*/
+/*jslint devel:true*/
 function fnHello() {
     "use strict";
     if (!document.getElementById("pPara")) {
-        var elPara1 = document.getElementById("elBody");
+        var elBody = document.getElementById("elBody");
         var elPara = document.createElement("p");
-        elPara1.appendChild(elPara);
+        elBody.appendChild(elPara);
         elPara.id = "pPara";
     }
-    document.getElementById("pPara").innerHTML = "Hello ";
-    document.getElementById("pPara").innerHTML = document.getElementById("pPara").innerHTML + document.getElementById("tUserInput").value;
+    var rad1 = document.getElementById("radAmin");
+    var rad2 = document.getElementById("radReza");
+    if (rad1.checked === true) {
+        document.getElementById("pPara").innerHTML = "The selected name is: " + rad1.value;
+    } else if (rad2.checked === true) { /*could have used else*/
+        document.getElementById("pPara").innerHTML = "The selected name is: " + rad2.value;
+    } else {
+        document.getElementById("pPara").innerHTML = "No name selected!";
+    }
 }
