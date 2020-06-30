fa3deff1e590272105802a3fc419bdfcf00eb2b3
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-06-30 11:35:49 +0430

    optimized switch case

--- jsCode.js
+++ jsCode.js
@@ -1,4 +1,6 @@
+/*jslint browser:true*/
 function printDay() {
+    "use strict";
     var body = document.getElementById("body");
     if (!document.getElementById("para")) {
         var resultElement = document.createElement("p");
@@ -6,32 +8,29 @@ function printDay() {
         body.appendChild(resultElement);
     }
     var day = document.getElementById("day1").value;
-    var dayText = document.createTextNode("");
 
-    var paragraph = document.getElementById("para");
- 
     switch (Number(day)) {
-        case 1:
-            var para1 = document.getElementById("para").innerHTML = "Day is: Saturday";
-            break;
-        case 2:
-            para1 = document.getElementById("para").innerHTML = "Day is: Sunday";
-            break;
-        case 3:
-            para1 = document.getElementById("para").innerHTML = "Day is: Monday";
-            break;
-        case 4:
-            para1 = document.getElementById("para").innerHTML = "Day is: Tuesday";
-            break;
-        case 5:
-            para1 = document.getElementById("para").innerHTML = "Day is: Wednesday";
-            break;
-        case 6:
-            para1 = document.getElementById("para").innerHTML = "Day is: Thursday";
-            break;
-        case 7:
-            para1 = document.getElementById("para").innerHTML = "Day is: Friday";
-            break;
+    case 1:
+        document.getElementById("para").innerHTML = "Day is: Saturday";
+        break;
+    case 2:
+        document.getElementById("para").innerHTML = "Day is: Sunday";
+        break;
+    case 3:
+        document.getElementById("para").innerHTML = "Day is: Monday";
+        break;
+    case 4:
+        document.getElementById("para").innerHTML = "Day is: Tuesday";
+        break;
+    case 5:
+        document.getElementById("para").innerHTML = "Day is: Wednesday";
+        break;
+    case 6:
+        document.getElementById("para").innerHTML = "Day is: Thursday";
+        break;
+    case 7:
+        document.getElementById("para").innerHTML = "Day is: Friday";
+        break;
     }
 
 }
