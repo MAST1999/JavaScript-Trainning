b01faae748ceff10b631641413b69024fd96a81e
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-01 13:17:57 +0430

    Form validation

--- jsCode.js
+++ jsCode.js
@@ -1,15 +1,31 @@
 /*jslint browser:true*/
 /*jslint devel:true*/
-function fnHello() {
+function validate() {
     "use strict";
-    if (!document.getElementById("pPara")) {
-        var elBody = document.getElementById("elBody");
-        var elPara = document.createElement("p");
-        elBody.appendChild(elPara);
-        elPara.id = "pPara";
+    var sError = "Please enter your ";
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    if (username.value.trim() === "" && password.value.trim() === "") {
+        sError = sError + "Username and Password";
+        alert(sError);
+        return false;
+    } else if (password.value.trim() === "" || password.value.trim().length < 8) {
+        sError = sError + "Password and make it longer than 8 characters";
+        password.style.border = "solid 3px red";
+        document.getElementById("laPassword").style.visibility = "visible";
+        alert(sError);
+        return false;
+    } else if (username.value.trim() === "" || username.value.length < 6) {
+        sError = sError + "Username and make it longer than 6 characters";
+        username.style.border = "solid 3px red";
+        document.getElementById("laUsername").style.visibility = "visible";
+        document.getElementById("laPassword").style.visibility = "hidden";
+        password.style.border = "";
+        alert(sError);
+        return false;
+    } else {
+        password.style.border = "";
+        username.style.border = "";
+        return true;
     }
-    // document.getElementById("pPara").innerHTML = "The selected name is: " + document.getElementById("selNames").value;
-    var elSelect = document.getElementById("selNames");
-    document.getElementById("pPara").innerHTML = "The selected name is: " + elSelect.options[elSelect.selectedIndex].value;
-    // my own way was better!!!
 }
