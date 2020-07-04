23b8ba2f59025e3f4ce3cb4e4f0207a20d3ed3f7
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-04 12:33:21 +0430

    More on regular expressions in the commments

--- jsCode.js
+++ jsCode.js
@@ -1,24 +1,38 @@
 /*jslint browser:true*/
-/*jslint devel: true*/
+/*jslint devel:true*/
 
 function validate() {
     "use strict";
-    var regExper = /[emi]00/i;
+    var regExper = /^\d{10}$/gi;
+    // ^ in the beginning of regExpress means the start.
+    // $ in the end means the end of regExpress.
     // var regExper = /[a-r]00/i;
     // var regExper = /[a-r]0[2-6]0[b-g]/i;
-    // for excluding var regExper = /[^1abc]0[a-r]0/i;;
+    // for excluding var regExper = /[^1abc]0[a-r]0/i;
     // regExper = new RegExper("E00"."i");
+    /*
+        \d - match any digit (equal to [0-9]);
+        \w - match any word character (a-z,A-Z,0-9 & ...);
+        \s - match whitespace character (eg - spaces & tabs);
+        \t - match a tab only;
+    */
+   // use () for separating parts of the regExpress (/^(a-z)(\d{2})$/).
+   // for . use : \.;
+   // you can give range to character : /^[a-z]{2,20}$/ or use /^[a-z]+$/ for one or more;
+   // you can use @ or . etc like this /^([a-z]+)@(a-z).(\d)$/;
+   // to make a part optional use ? : /^([a-z]+)@(a-z).(\d)?$/;
+
     var sError = "Please enter your ";
-    var username = document.getElementById("username");
+    var phoneNumber = document.getElementById("phoneNumber");
     var password = document.getElementById("password");
     var pass_border = "";
     var user_border = "";
     document.getElementById("laPassword").style.visibility = "hidden";
     document.getElementById("laUsername").style.visibility = "hidden";
     password.style.border = pass_border;
-    username.style.border = user_border;
-    if (username.value.trim() === "" && password.value.trim() === "") {
-        sError = sError + "Username and Password";
+    phoneNumber.style.border = user_border;
+    if (phoneNumber.value.trim() === "" && password.value.trim() === "") {
+        sError = sError + "PhoneNumber and Password";
         alert(sError);
         return false;
     }
@@ -29,14 +43,14 @@ function validate() {
         alert(sError);
 
     }
-    if (username.value.trim() === "" || username.value.length < 6 || !regExper.test(username.value)) {
-        sError = sError + "Username and make it longer than 6 characters and must contain A01";
+    if (phoneNumber.value.trim() === "" || !regExper.test(phoneNumber.value)) {
+        sError = sError + "Phone Number and make it 10 numbers";
         user_border = "solid 3px red";
         document.getElementById("laUsername").style.visibility = "visible";
         alert(sError);
     }
     password.style.border = pass_border;
-    username.style.border = user_border;
+    phoneNumber.style.border = user_border;
     if (pass_border || user_border) {
         return false;
     }
