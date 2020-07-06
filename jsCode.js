d778f77946ac58c8cbb6d506953ff1e7a5594425
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-06 20:05:44 +0430

    JQuery selectors part 2

--- jsCode.js
+++ jsCode.js
@@ -7,6 +7,9 @@
 function fn1() {
     "use strict";
     //$("h2").fadeToggle(250); the selection is the same as CSS!
-    $("#elH2").fadeToggle(250);
-    $(".someHeadings").fadeToggle(500);
+    //$("#secondDiv,li").fadeToggle(250);
+    //$("div > ul").fadeToggle(250); //$("div p").fadeToggle(250); works the same! same as CSS!
+    //$(".divs").fadeToggle(250);
+    $("p:first").fadeToggle(250); // same can be done with classes.
+    $("li:even").fadeToggle(250); // even li's fade!
 }
