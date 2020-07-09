ebd9afbdf04c1454eb41057775de3e6bff4a8407
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-09 15:42:18 +0430

    JQuery DOM Manipulations Part 3

--- jsCode.js
+++ jsCode.js
@@ -4,13 +4,22 @@
     window, $
 */
 
-$(document).ready(function (){
-    function fn1(){
+$(document).ready(function () {
+    function fn1() {
         //$("#p1").text("hello world");
         //alert($("#p1").html());
         //$("#p1").html("this is <strong>my bad<strong/>");
         //alert($("#div1").attr("id"));
-        alert($("#div1").css("font-size"));
+        $("#p1").css("font-size", "30px");
+    /*this can be done with attr as well*/
+        $("#div1").attr("class", "divClass1");
+        $("#div1").css("border", "solid 3px blue");
     }
+    function fn2() {
+        alert($("#p1").css("font-size"));
+        alert($("#div1").attr("class"));
+    }
+
     $("#btnClick1").click(fn1);
+    $("#btnClick2").click(fn2);
 });
