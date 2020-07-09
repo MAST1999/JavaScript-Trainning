cfcf5a0db05e48d6bf3fd8dffeda2d0c0ef9a92e
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-09 16:59:36 +0430

    JQuery DOM Manipulation Part 3

--- jsCode.js
+++ jsCode.js
@@ -5,21 +5,31 @@
 */
 
 $(document).ready(function () {
-    function fn1() {
-        //$("#p1").text("hello world");
-        //alert($("#p1").html());
-        //$("#p1").html("this is <strong>my bad<strong/>");
-        //alert($("#div1").attr("id"));
-        $("#p1").css("font-size", "30px");
-    /*this can be done with attr as well*/
-        $("#div1").attr("class", "divClass1");
-        $("#div1").css("border", "solid 3px blue");
+    function append1() {
+        $("#div1").append("<p>Hello world<p/>");
+        //$("#btnClick1").css("visibility", "hidden");
+        //$("#btnClick1").fadeOut();
     }
-    function fn2() {
-        alert($("#p1").css("font-size"));
-        alert($("#div1").attr("class"));
+    function prepend1() {
+        $("#div1").prepend("<p>Hello world<p/>");
+    }
+    function after1() {
+        $("#div1").after("<p>Hello world<p/>");
+    }
+    function before1() {
+        $("#div1").before("<p>Hello world<p/>");
+    }
+    function remove1() {
+        $("p").remove("#p2, #p1");
+    }
+    function empty1() {
+        $("p").empty();
     }
 
-    $("#btnClick1").click(fn1);
-    $("#btnClick2").click(fn2);
+    $("#btnClick1").click(append1);
+    $("#btnClick2").click(prepend1);
+    $("#btnClick3").click(after1);
+    $("#btnClick4").click(before1);
+    $("#btnClick5").click(remove1);
+    $("#btnClick6").click(empty1);
 });
