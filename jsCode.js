752393a2883c53583719bd21e2a43c99fc71855f
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-11 01:06:04 +0430

    Fixed ESlinter errors and added JQueryUI

--- jsCode.js
+++ jsCode.js
@@ -4,32 +4,25 @@
     window, $
 */
 
-$(document).ready(function () {
-    function append1() {
-        $("#div1").append("<p>Hello world<p/>");
-        //$("#btnClick1").css("visibility", "hidden");
-        //$("#btnClick1").fadeOut();
+
+$(function () {
+    function addClass() {
+        $("#div1").addClass("divStyled");
     }
-    function prepend1() {
-        $("#div1").prepend("<p>Hello world<p/>");
+    function removeClass() {
+        $("#div1").removeClass("divStyled");
     }
-    function after1() {
-        $("#div1").after("<p>Hello world<p/>");
+    function toggleClass() {
+        $("#div1").toggleClass("divStyled pStyled");
     }
-    function before1() {
-        $("#div1").before("<p>Hello world<p/>");
-    }
-    function remove1() {
-        $("p").remove("#p2, #p1");
-    }
-    function empty1() {
-        $("p").empty();
+    function CSS1() {
+        var padding = $("#div1").css("padding-left");
+        $("#p2").html(padding);
+
     }
 
-    $("#btnClick1").click(append1);
-    $("#btnClick2").click(prepend1);
-    $("#btnClick3").click(after1);
-    $("#btnClick4").click(before1);
-    $("#btnClick5").click(remove1);
-    $("#btnClick6").click(empty1);
+    $("#btnClick1").on("click", addClass);
+    $("#btnClick2").on("click", removeClass);
+    $("#btnClick3").on("click", toggleClass);
+    $("#btnClick4").on("click", CSS1);
 });
