7e0a2101b1d41f700dedb016b5b30c9061c18d82
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-11 18:18:41 +0430

    Accordion Widget Control in JQuery UI

--- jsCode.js
+++ jsCode.js
@@ -6,14 +6,14 @@
 
 
 $(function () {
-    $("#img1").tooltip({
-        track: true,
-        content: "TVs are expensive",
-        show:{
-            effect:"bounce", duration:900 //other effect's consists of "explode", "blind"
-        },
-        hide: {
-            effect: "blind", duration: 900
-        }
+    $("#panel").accordion({
+        collapsible:true,
+        event: "click",
+        animate:500,
+        active:0,
+        heightStyle:true,    /*if one of the panels has a lot more content we 
+                            use this to make sure it resizes it self 
+                            to look nice with the rest of the panels*/
+        icons:{header:"ui-icon-plus", activeHeader:"ui-icon-minus"}
     });
 });
