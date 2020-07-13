d1ef4661047e036d98b8efc2320a9bc61b79f2f1
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2020-07-13 16:11:40 +0430

    Draggable Interaction in JQuery UI

--- jsCode.js
+++ jsCode.js
@@ -6,40 +6,13 @@
 
 
 $(function () {
-    var foods = [
-        "sandwich",
-        "pizza",
-        "bread"
-    ];
-    $("#foodSelector").autocomplete({
-        source: foods //first bracket if for source
-    }, {
-        autoFocus:true, //the second bracket is for the other things
-        delay:500,
-        minLength:2
+    $(".cube").draggable({
+        axis: "x", // or y.
+        containment:"parent",
+        cursor: "grabbing",
+        opacity: 0.5,
+        //grid: [300, 300],
+        snap: true,
+        snapTolerance:30
     });
-
-    $("#div2").accordion({
-        collapsible: true,
-        active: 0
-    });
-
-    $("#div3").dialog({
-        modal: false,
-        width: "400",
-        height: "300",
-        resizable: false
-    });
-
-    function getter() {
-        var height = $("#div3").dialog("option", "height"); //getter
-        alert(height);
-    }
-    function setter() {
-        $("#div3").dialog("option", "height", "600"); //setter
-    }
-
-    $("#btnGet").on("click", getter);
-    $("#btnChange").on("click", setter);
-    
 });
